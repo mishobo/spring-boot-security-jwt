fe93521fda0c7520d78abaccc107662bb281dd7d
mishobo <60912349+mishobo@users.noreply.github.com>
2022-10-17 12:05:38 +0300

    spring security configurations

---
+++ src/main/java/com/husseinabdallah/authenticationApp/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.husseinabdallah.authenticationApp.security.services;
+
+import com.husseinabdallah.authenticationApp.models.User;
+import com.husseinabdallah.authenticationApp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
